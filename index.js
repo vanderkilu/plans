663e49999da2f08368177bdd0b49f16aeab1a339
vanderkilu <vanderkiliku@gmail.com>
2020-05-04 13:15:32 +0000

    initial setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const ROOT_API = "http://www.mocky.io/v2/5eb0053d3300002b00c68ae9";
+
+const subscribe = async type => {
+  const response = await fetch(`${ROOT_API}/${type}`);
+  if (response.ok) {
+    alert(`You have successfully subscribed to ${type}`);
+  } else {
+    alert(`HTTP-Error:  ${response.status}`);
+  }
+};
